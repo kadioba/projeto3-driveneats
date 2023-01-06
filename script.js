a373d3a4aeb7ec60b917355dc0bf95174f4822b1
Vinicius Costa Predebon <kadiobaoff@gmail.com>
2023-01-06 11:39:49 -0300

    Versao  005 (Implementacao de selecao a bebidas)

--- script.js
+++ script.js
@@ -9,3 +9,27 @@ function selecionarPrato(pratoSelecionado){
     pratoSelecionado.classList.add("selecionado");
 
 }
+
+function selecionarBebida(bebidaSelecionada){
+
+    const bebidaDesmarcar = document.querySelector(".bebidas .selecionado");
+
+    if (bebidaDesmarcar !== null){
+        bebidaDesmarcar.classList.remove("selecionado");
+    }
+
+    bebidaSelecionada.classList.add("selecionado");
+
+}
+
+function selecionarPrato(pratoSelecionado){
+
+    const pratoDesmarcar = document.querySelector(".pratos .selecionado");
+
+    if (pratoDesmarcar !== null){
+        pratoDesmarcar.classList.remove("selecionado");
+    }
+
+    pratoSelecionado.classList.add("selecionado");
+
+}
