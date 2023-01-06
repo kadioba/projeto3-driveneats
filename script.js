af5fd9f1b07ffaea282ceb43274368b19c194607
Vinicius Costa Predebon <kadiobaoff@gmail.com>
2023-01-06 11:35:58 -0300

    Versao 004 (Selecao exclusiva de pratos implementada)

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function selecionarPrato(pratoSelecionado){
+
+    const pratoDesmarcar = document.querySelector(".pratos .selecionado");
+
+    if (pratoDesmarcar !== null){
+        pratoDesmarcar.classList.remove("selecionado");
+    }
+
     pratoSelecionado.classList.add("selecionado");
 
 }
